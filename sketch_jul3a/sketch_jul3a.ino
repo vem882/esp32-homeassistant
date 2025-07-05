bced08a95d63069b1854c618acc122f9c6f562f5
Martin Negin <vem882@gmail.com>
2025-07-05 04:25:48 +0300

    Bump firmware version to 1.0.1
    Updated the FIRMWARE_VERSION macro from 1.0.0 to 1.0.1 to reflect the latest firmware release.

--- sketch_jul3a/sketch_jul3a.ino
+++ sketch_jul3a/sketch_jul3a.ino
@@ -10,7 +10,7 @@
 #include <Update.h>
 
 // Firmware version and OTA configuration
-#define FIRMWARE_VERSION "1.0.0"
+#define FIRMWARE_VERSION "1.0.1"
 #define OTA_UPDATE_URL "https://raw.githubusercontent.com/vem882/esp32-homeassistant/main/version.json"
 #define OTA_FIRMWARE_URL "https://raw.githubusercontent.com/vem882/esp32-homeassistant/main/firmware.bin"
 
